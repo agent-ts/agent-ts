7e70d79bf926dd2eb9af0342b38a2d4921886c97
Wei Wei <shazhouke@gmail.com>
2025-04-14 20:46:07 +0800

    Implement a simple agent

--- packages/agent/index.ts
+++ packages/agent/index.ts
@@ -1,34 +1,61 @@
-import type { Tool } from "@agent-ts/tool";
-import { makeTool } from "@agent-ts/tool";
-import type { AgentContext, AgentOutput } from "./types";
-import { schema } from "@agent-ts/schema";
+import type { OpenAI } from "openai";
+import { makeTool, type Tool } from "@agent-ts/tool";
+import { schema, type Schema, type ValueOf } from "@agent-ts/schema";
 
-const agent = makeTool({
-  name: "agent",
-  description: "Agent",
-  inputSchema: schema({
-    type: 'object',
+const agent = ({
+  openAI,
+  model,
+}: {
+  openAI: OpenAI;
+  model: string;
+}) => {
+  const inputSchema = schema({
+    type: "object",
     properties: {
       message: {
-        type: 'string',
+        type: "string",
       },
     },
-    required: ['message'],
-  }),
-  outputSchema: schema({
-    type: 'object',
+    required: ["message"] as const,
+  });
+
+  const outputSchema = schema({
+    type: "object",
     properties: {
       type: {
-        type: 'string',
-        enum: ['complete', 'error', 'continue'],
+        type: "string",
+        enum: ["complete", "error", "continue"] as const,
       },
       message: {
-        type: 'string',
+        type: "string",
       },
     },
-    required: ['type', 'message'],
-  }),
-  invoke: async (parameters: { message: string }, trace: (chunk: string) => void) => {
-    return { type: "complete", message: "Hello, world!" };
-  },
-});
+    required: ["type", "message"] as const,
+  });
+
+  const agentTool: Tool = makeTool({
+    name: "agent",
+    description: "Agent",
+    inputSchema,
+    outputSchema,
+    invoke: async (
+      parameters: ValueOf<typeof inputSchema>,
+      trace: (chunk: string) => void
+    ) => {
+      const response = await openAI.chat.completions.create({
+        model,
+        messages: [{ role: "user", content: parameters.message }],
+        tools: [agentTool.definition],
+      });
+
+      return {
+        type: "complete" as const,
+        message: response?.choices?.[0]?.message.content ?? "",
+      };
+    },
+  });
+
+  return agentTool;
+};
+
+export default agent;
